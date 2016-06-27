e2508f4d8e9289964400d02b81baa8e40e7b9c16
giginet <giginet.net@gmail.com>
2016-06-27 21:10:15 +0900

    Modify build setting

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -40,9 +40,9 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("CCSocialShare-Android", Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+        glview = GLViewImpl::createWithRect("CCSocialShareAndroid", Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
 #else
-        glview = GLViewImpl::create("CCSocialShare-Android");
+        glview = GLViewImpl::create("CCSocialShareAndroid");
 #endif
         director->setOpenGLView(glview);
     }
--- proj.android-studio/app/jni/Android.mk
+++ proj.android-studio/app/jni/Android.mk
@@ -12,9 +12,11 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../../Classes/AppDelegate.cpp \
-                   ../../../Classes/HelloWorldScene.cpp
+                   ../../../Classes/HelloWorldScene.cpp \
+                   ../../../Classes/external/CCSocialShare/CCSocialShare/android/CCSocialManager.cpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes \
+                    $(LOCAL_PATH)/../../../Classes/external/CCSocialShare/CCSocialShare
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
